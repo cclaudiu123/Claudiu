4b90652e2eff3b948c7cfb28e4022942925be82c
cclaudiu123 <caprarclaudiu93@yahoo.com>
2016-08-04 13:34:12 +0300

    Add files via upload

---
+++ Test/inc/update.inc.php
@@ -0,0 +1,28 @@
+<?php
+if(($_SERVER['REQUEST_METHOD']=="POST") && $_POST['submit']=="Save Entry"
+    && !empty($_POST['page'])
+    && !empty($_POST['title'])
+    && !empty($_POST['entry']))
+{
+    include_once 'db.inc.php';
+    $db = new PDO(DB_INFO, DB_USER, DB_PASS);
+    //save entry to database
+    $sql = "INSERT INTO entries (page,title,entry) VALUES (?,?,?)";
+    $stmt = $db->prepare($sql);
+    $stmt->execute(array($_POST['page'],$_POST['title'],$_POST['entry']));
+    $stmt->closeCursor();
+    //sanitize information
+    $page = htmlentities(strip_tags($_POST['page']));
+    //get id of the last inserted entry
+    $id_obj = $db->query("SELECT LAST_INSERT_ID()");
+    $id = $id_obj->fetch();
+    $id_obj->closeCursor();
+    //send the user to the new entry
+    header('Location: /?page='.$page.'&id='.$id[0]);
+    exit;
+}
+else {
+    header('Location:../');
+    exit;
+}
+?>
